4a492ba12f205d74de923953cb3aea1babd55c8c
Zespier <pabloreco.redondo@gmail.com>
2024-02-11 00:29:41 +0100

    Inserted the wowlf and tried his behaviour

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,21 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.VersionControl.Asset;
 
 public class Enemy : Damageable {
 
+    [Header("Enemy Base Attributes")]
+
+    public Rigidbody2D rb;
     public float speed = 10f;
     public Transform player;
+    public SpriteRenderer sprite;
+
+    public virtual float Speed => speed;
 
     [Header("Damageable")]
-    public float hp;
+    public float hp = 1000f;
 
-    private void Update() {
-        Movement();
+    [Header("States")]
+    public List<AttackBaseState> states;
+    public AttackBaseState currentState;
+
+    private void Start() {
+        currentState.OnStateEnter();
     }
 
-    private void Movement() {
-        transform.position += Time.deltaTime * speed * (player.position - transform.position).normalized;
+    private void Update() {
+        FlipSprite();
+        currentState.StateUpdate();
+    }
+
+    private void LateUpdate() {
+        currentState.StateLateUpdate();
+    }
+
+    //private void Movement() {
+    //    Vector3 step = Time.deltaTime * speed * (player.position - transform.position).normalized;
+    //    transform.position += step;
+    //}
+
+    private void FlipSprite() {
+        sprite.flipX = (player.position - transform.position).x < 0;
+    }
+
+    public virtual void ChangeState(Type state) {
+        if (!states.Exists(s => s.GetType() == state)) {
+            Debug.LogError("This state is not registered");
+            return;
+        }
+
+        currentState.OnStateExit();
+        currentState = states.Find(s => s.GetType() == state);
+        currentState.OnStateEnter();
     }
 
     #region Taking damage and deactivation
