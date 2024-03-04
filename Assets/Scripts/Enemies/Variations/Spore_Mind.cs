ecbac0034bee3485c9ff34062aa54abebf0557c9
Roundev <pabloreco.redondo@gmail.com>
2024-03-04 14:02:21 +0100

    Spore_Zone

--- Assets/Scripts/Enemies/Variations/Spore_Mind.cs
+++ Assets/Scripts/Enemies/Variations/Spore_Mind.cs
@@ -8,6 +8,20 @@ public class Spore_Mind : MonoBehaviour {
     public GameObject spore_Big;
     public GameObject spore_Normal;
     public GameObject spore_Small;
+    public Spore_Zone spore_Zone;
+
+    public float spore_Zone_MediumSize = 1.5f;
+    public float spore_Zone_BigSize = 2f;
+    public float spore_Zone_SpawningTime = 3f;
+    public float spore_Zone_lifetime = 10f;
+
+    [Header("Plague Control")]
+    public int totalSporeZones;
+
+    public static Spore_Mind instance;
+    private void Awake() {
+        if (!instance) { instance = this; }
+    }
 
     private void OnEnable() {
         Events.OnEnemyDeath += SporeDeath;
@@ -17,27 +31,33 @@ public class Spore_Mind : MonoBehaviour {
         Events.OnEnemyDeath -= SporeDeath;
     }
 
+    public void SpawnSpore(GameObject prefab, Vector3 position) {
+        Instantiate(prefab, position, Quaternion.identity, EnemyContainer.instance.transform);
+    }
+
+    public void Spore_Zone(Vector3 position, Spore_ZoneSize spore_ZoneSize) {
+
+        Spore_Zone newSpore_zone = Instantiate(spore_Zone, position, Quaternion.identity, EnemyContainer.instance.transform);
+        newSpore_zone.Size = spore_ZoneSize;
+    }
+
     public void SporeDeath(Enemy spore) {
 
         Enum.TryParse(spore.GetType().ToString(), out SporeType sporeType);
 
         switch (sporeType) {
-            case SporeType.Spore_Big:
-                Instantiate(spore_Normal, spore.transform.position, Quaternion.identity, EnemyContainer.instance.transform);
-                break;
-
-            case SporeType.Spore_Normal:
-                Instantiate(spore_Small, spore.transform.position, Quaternion.identity, EnemyContainer.instance.transform);
-                break;
-
-            case SporeType.Spore_Small:
-                //Instantiate(spore_Small, spore.transform.position, Quaternion.identity, EnemyContainer.instance.transform);
-                break;
-
             case SporeType.none:
-                //Instantiate(spore_Small, spore.transform.position, Quaternion.identity, EnemyContainer.instance.transform);
                 break;
-
+            case SporeType.Spore_Small:
+                break;
+            case SporeType.Spore_Normal:
+                SpawnSpore(spore_Small, spore.transform.position);
+                break;
+            case SporeType.Spore_Big:
+                SpawnSpore(spore_Normal, spore.transform.position);
+                SpawnSpore(spore_Normal, spore.transform.position);
+                Spore_Zone(spore.transform.position, Spore_ZoneSize.big);
+                break;
             default:
                 break;
         }
@@ -46,7 +66,8 @@ public class Spore_Mind : MonoBehaviour {
 
 public enum SporeType {
     none,
-    Spore_Big,
-    Spore_Normal,
     Spore_Small,
+    Spore_Normal,
+    Spore_Big,
 }
+
---
+++ Assets/Scripts/Enemies/Variations/Spore_Zone.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spore_Zone : MonoBehaviour {
+
+    [SerializeField] private Spore_ZoneSize size;
+    public Transform body;
+    public CircleCollider2D circleCollider2D;
+
+    private float _defaultBodySize;
+    private float _defaultCollider2DRadius;
+
+    private float _spawnTimer;
+    private float _lifetimeTimer;
+    private float _sizeRatio = 1f;
+
+    public Spore_ZoneSize Size { get => size; set { size = value; SetSize(); } } //Makes sense that everytime we change the size, it ACTUALLY changes the size
+
+    private void Awake() {
+        _defaultBodySize = body.localScale.x;
+        _defaultCollider2DRadius = circleCollider2D.radius;
+    }
+
+    private void OnEnable() {
+        Spore_Mind.instance.totalSporeZones++;
+    }
+
+    private void OnDisable() {
+        Spore_Mind.instance.totalSporeZones--;
+    }
+
+    private void Update() {
+
+        if (_spawnTimer >= (Spore_Mind.instance.spore_Zone_SpawningTime - ReduceSpawnTimeBasedOnSpore_Zones()) / _sizeRatio) {
+            _spawnTimer = 0;
+            Spore_Mind.instance.SpawnSpore(Spore_Mind.instance.spore_Small, transform.position);
+        }
+
+        if (_lifetimeTimer >= Spore_Mind.instance.spore_Zone_lifetime) {
+            Destroy(gameObject);
+        }
+
+        _spawnTimer += Time.deltaTime;
+        _lifetimeTimer += Time.deltaTime;
+    }
+
+    public float ReduceSpawnTimeBasedOnSpore_Zones() {
+
+        int totalSpawnZones = Spore_Mind.instance.totalSporeZones > 30 ? 30 : Spore_Mind.instance.totalSporeZones;
+        return Mathf.Log(totalSpawnZones, Mathf.Exp(1) + 1);
+    }
+
+    public void SetSize() {
+
+        switch (size) {
+
+            case Spore_ZoneSize.small:
+                //TODO: Change size
+                break;
+
+            case Spore_ZoneSize.medium:
+                ProportionalSize(Spore_Mind.instance.spore_Zone_MediumSize);
+                break;
+
+            case Spore_ZoneSize.big:
+                ProportionalSize(Spore_Mind.instance.spore_Zone_BigSize);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void ProportionalSize(float ratio) {
+        _sizeRatio = ratio;
+        body.localScale = Vector3.one * _defaultBodySize * ratio;
+        circleCollider2D.radius = _defaultCollider2DRadius * ratio;
+    }
+
+}
+
+public enum Spore_ZoneSize {
+    small,
+    medium,
+    big,
+}
