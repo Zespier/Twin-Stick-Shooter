dd7757c78d619fc7260fe772b3769e54b0cc0158
Zespier <pabloreco.redondo@gmail.com>
2024-03-06 13:13:55 +0100

    Spore_Egg Added

--- Assets/Scripts/Enemies/Variations/Spore_Zone.cs
+++ Assets/Scripts/Enemies/Variations/Spore_Zone.cs
@@ -3,26 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spore_Zone : MonoBehaviour {
+public class Spore_Zone : Generator {
 
-    [SerializeField] private Spore_ZoneSize size;
-    public Transform body;
-    public CircleCollider2D circleCollider2D;
-
-    private float _defaultBodySize;
-    private float _defaultCollider2DRadius;
-
-    private float _spawnTimer;
-    private float _lifetimeTimer;
-    private float _sizeRatio = 1f;
-
-    public Spore_ZoneSize Size { get => size; set { size = value; SetSize(); } } //Makes sense that everytime we change the size, it ACTUALLY changes the size
-
-    private void Awake() {
-        _defaultBodySize = body.localScale.x;
-        _defaultCollider2DRadius = circleCollider2D.radius;
-    }
-
+    public override GameObject SporePrefab { get => Spore_Mind.instance.spore_Small; }
 
     private void OnEnable() {
         StartCoroutine(C_WaitForSpore_MindInstantiation(1, result => { Spore_Mind.instance.totalSpore_Zones += result; }));
@@ -44,58 +27,4 @@ public class Spore_Zone : MonoBehaviour {
         action?.Invoke(value);
     }
 
-    private void Update() {
-
-        if (_spawnTimer >= (Spore_Mind.instance.spore_Zone_SpawningTime - ReduceSpawnTimeBasedOnSpore_Zones()) / _sizeRatio) {
-            _spawnTimer = 0;
-            Spore_Mind.instance.SpawnSpore(Spore_Mind.instance.spore_Small, transform.position);
-        }
-
-        if (_lifetimeTimer >= Spore_Mind.instance.spore_Zone_lifetime) {
-            Destroy(gameObject);
-        }
-
-        _spawnTimer += Time.deltaTime;
-        _lifetimeTimer += Time.deltaTime;
-    }
-
-    public float ReduceSpawnTimeBasedOnSpore_Zones() {
-
-        int totalSpawnZones = Spore_Mind.instance.totalSpore_Zones > 30 ? 30 : Spore_Mind.instance.totalSpore_Zones;
-        return Mathf.Log(totalSpawnZones, Mathf.Exp(1) + 1);
-    }
-
-    public void SetSize() {
-
-        switch (size) {
-
-            case Spore_ZoneSize.small:
-                //TODO: Change size
-                break;
-
-            case Spore_ZoneSize.medium:
-                ProportionalSize(Spore_Mind.instance.spore_Zone_MediumSize);
-                break;
-
-            case Spore_ZoneSize.big:
-                ProportionalSize(Spore_Mind.instance.spore_Zone_BigSize);
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    public void ProportionalSize(float ratio) {
-        _sizeRatio = ratio;
-        body.localScale = Vector3.one * _defaultBodySize * ratio;
-        circleCollider2D.radius = _defaultCollider2DRadius * ratio;
-    }
-
-}
-
-public enum Spore_ZoneSize {
-    small,
-    medium,
-    big,
 }
