55924d7b3bf8eb2fd297d1ed6602ae0bc998bd75
Roundev <pabloreco.redondo@gmail.com>
2024-03-11 11:44:23 +0100

    Separated Script

--- Assets/ZespierTools/Shooter/BulletFireDesviation.cs
+++ Assets/ZespierTools/Shooter/BulletFireDesviation.cs
@@ -42,29 +42,3 @@ public enum ShootDirectionReference {
     up,
     right
 }
-
-public class RandomPosition {
-
-    public static Vector2 Square_2D(float radius) {
-
-        return new Vector2(Random.Range(-radius, radius), Random.Range(-radius, radius));
-    }
-
-    public static Vector2 Circle_2D(float radius, bool clampedToCircunference) {
-
-        Vector2 newPosition = new Vector2(Random.Range(-radius, radius), Random.Range(-radius, radius));
-
-
-        if (clampedToCircunference) {
-            return newPosition.normalized * radius;
-
-        } else {
-
-            if (newPosition.sqrMagnitude > radius * radius) {
-                return newPosition.normalized * radius;
-            }
-
-            return newPosition;
-        }
-    }
-}
